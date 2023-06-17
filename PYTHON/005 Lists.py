f4f66b4b0a7166f5309fa5c89a27707a38cb8b32
Ruslan Bulatov <rf011@yandex.ru>
2023-06-17 23:57:02 +0300

    add 005 Lists

---
+++ PYTHON/005 Lists.py
@@ -0,0 +1,24 @@
+personal = ['Alex','Ivan','Ira','Mary']
+print(personal[0])   #print 'Alex'
+
+personal = ['Alex','Ivan','Ira','Mary']
+print(len(personal))    #count number of items
+
+personal = ['Alex','Ivan','Ira','Mary']
+print(personal[2])   #print last item
+
+personal = ['Alex','Ivan','Ira','Olga']
+print(personal[0:2])    #print from beginning 2 items
+
+personal = ['Alex','Ivan','Ira','Olga']
+print(personal[1:])     #print since 2 item all times
+
+personal.append('Fedor')
+print(personal) 	#add item 'Fedor'
+
+personal = ['Alex','Ivan','Ira','Olga']
+num = [1, 2, 3, 4]
+pn = []
+pn.append(personal)
+pn.append(num)
+print(pn)       #sum of some Lists
