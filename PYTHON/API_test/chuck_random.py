c90db42ee2be98587c7ce1b0d30f0de284eb12d3
Ruslan Bulatov <rf011@yandex.ru>
2023-06-18 20:34:10 +0300

    add Chuck random joke

---
+++ PYTHON/API_test/chuck_random.py
@@ -0,0 +1,38 @@
+import requests
+
+class Test_new_joke():
+    """Create new joke"""
+
+    def __init__(self):
+        pass
+
+    def test_create_new_random_joke(self):
+        """Create random joke"""
+
+        url = 'https://api.chucknorris.io/jokes/random'
+        print(url)
+        result = requests.get(url)
+        print('Status code: ' + str(result.status_code))
+        assert 200 == result.status_code
+        if result.status_code == 200:
+            print('Success')
+        else:
+            print('Fail')
+        result.encoding = 'utf-8'
+        print(result.text)
+        check = result.json()
+        # check_info = check.get('categories')
+        # print(check_info)
+        # assert check_info == []
+        # print("Category is right")
+        check_info_value = check.get('value')
+        print(check_info_value)
+        name = 'Chuck'
+        if name in check_info_value:
+            print('value has word Chuck')
+        else:
+            print('valeu has no word Chuck')
+
+
+random_joke = Test_new_joke()
+random_joke.test_create_new_random_joke()
