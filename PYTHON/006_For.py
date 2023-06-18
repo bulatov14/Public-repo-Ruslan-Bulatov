0b4dcb970af68d9316369790ea1f82ddfa049846
Ruslan Bulatov <rf011@yandex.ru>
2023-06-18 10:18:16 +0300

    add For cycle

---
+++ PYTHON/006_For.py
@@ -0,0 +1,30 @@
+# students = ['Alex','Ivan','Ira','Semen','Igor','Sveta']
+# for f in students:
+#     print(f) #Alex, Ivan, ...
+
+# students = ['Alex','Ivan','Ira','Semen','Igor','Sveta']
+# for f in students:
+#     var = 'Engineer ' + f
+#     print(var) #Engineer Alex, Engineer Ivan, ...
+
+# students = ['Alex','Ivan','Ira','Semen','Igor','Sveta']
+# for f in students:
+#     if f == 'Ira':
+#         var = 'Engineer ' + f
+#         print(var) #Engineer Ira
+
+# students = ['Alex','Ivan','Ira','Semen','Igor','Sveta']
+# for f in students[:3]:
+#     print(f) #print only first 3 items
+
+# students = ['Alex','Ivan','Ira','Semen','Igor','Sveta']
+# for f in students[3:]:
+#     print(f) #print only last 3 items
+
+# students = ['Alex','Ivan','Ira','Semen','Igor','Sveta']
+# for f in students[1:4]:
+#     print(f) #print only 1-3 items
+
+students = ['Alex','Ivan','Ira','Semen','Igor','Sveta']
+for f in students:
+    print(len(f)) #print number of letters in each item
