0e9d4d4f091755465cd4255f1f82c525976c6788
Ruslan Bulatov <rf011@yandex.ru>
2023-06-18 18:16:53 +0300

    add test_mail_1 & 2

---
+++ PYTHON/Pytest/tests/test_mail_1.py
@@ -0,0 +1,10 @@
+import pytest
+
+@pytest.fixture()
+def set_up():
+    print('Setup is successful')   # fixture - действие ДО - действия будут выполнены ДО того как мы запустим каждый наш модуль
+def test_sending_mail_1(set_up):
+    print('Letter is sent')     # input 'pytest -s -v' in the terminal
+
+def test_sending_mail_2(set_up):
+    print('Letter is sent')
