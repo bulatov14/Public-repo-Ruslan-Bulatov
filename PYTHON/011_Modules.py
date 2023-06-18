b8fa02f522a4330dee3cf6739d8191a46755035b
Ruslan Bulatov <rf011@yandex.ru>
2023-06-18 13:11:29 +0300

    add 011_Modules

---
+++ PYTHON/011_Modules.py
@@ -0,0 +1,23 @@
+
+    # Модули бывают самописные и с различных библиотек python
+
+    # файлы hello и start для этого урока с функцией import
+
+# def some():
+#     print('Hello world')
+# some()
+
+# def some():
+#     print('Hello')
+
+# import math
+# print(math.pi)
+
+# import random
+# r = random.randrange(0, 10)
+# print(r)        # random integer 0-10
+
+import random
+r = random.randrange(0, 1000000)
+user = 'User'
+print(user + str(r))
---
+++ PYTHON/start.py
@@ -0,0 +1,14 @@
+# import hello
+# hello.some()
+
+# import math
+# print(math.pi)
+
+# import random
+# r = random.randrange(0, 10)
+# print(r)        # random integer 0-10
+
+import random
+r = random.randrange(0, 1000000)
+user = 'User'
+print(user + str(r))
