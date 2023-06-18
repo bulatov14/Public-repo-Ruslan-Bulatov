2486de98a86be2a02f83df01f10225b09b9a8dd6
Ruslan Bulatov <rf011@yandex.ru>
2023-06-18 17:10:45 +0300

    add 016 Import class

---
+++ PYTHON/016_Import_class.py
@@ -0,0 +1,28 @@
+class Person():
+    """Создаем человека"""
+    def __init__(self, name, age, height):
+        """Инициализируем атрибуты человека"""
+
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = 100
+
+    def description_person(self):
+        """Получение описания человека"""
+
+        description = self.name + ', возраст ' + str(self.age) + ', рост ' + str(self.height) + ', вес ' + str(self.weight)
+        print('Нового человека зовут ', description)
+
+    def get_weight(self):
+        """Получение веса человека"""
+
+        print('Вес человека: ' + str(self.weight))
+
+    def update_weight(self, kg):
+        """Изменение веса человека"""
+
+        self.weight = kg
+
+man = Person('Alex', 30, 180)
+man.description_person()
---
+++ PYTHON/main.py
@@ -0,0 +1,9 @@
+
+    # for 016_Import_class
+
+from base_person import Person
+
+# from base_person import *    # так можно импортить все классы из base_person
+
+man = Person('Alex', 30, 180)
+man.description_person()
