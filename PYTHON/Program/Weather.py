f1c5c40b00a25846aa5c38dd777c367778645536
Ruslan Bulatov <rf011@yandex.ru>
2023-06-19 21:26:27 +0300

    add Weather app

---
+++ PYTHON/Program/Weather.py
@@ -0,0 +1,27 @@
+from pyowm import OWM
+# from pyowm.utils import config
+# from pyowm.utils import timestamps
+
+# ---------- FREE API KEY examples ---------------------
+
+owm = OWM('bb5024f7d713d3944ab88fdada3582cc')
+mgr = owm.weather_manager()
+
+place = input('Input your City: ')
+#
+# # Search for current weather in London (Great Britain) and get details
+# observation = mgr.weather_at_place(place)
+# w = observation.weather
+
+# Search for current weather in London (Great Britain) and get details
+observation = mgr.weather_at_place(place)
+w = observation.weather
+temp = w.temperature('celsius') ['temp']
+print('In ' + place + ' now is ' + str(w.detailed_status) + '. Humidity is ' + str(w.humidity) + '. Temperature is ' + str(temp))
+
+if temp < 10:
+    print('Now is cold. Put on warm coat')
+elif temp < 20:
+    print('The weather is cool')
+else:
+    print('Great weather ))')
